46658ffe9da2b6359f7ab4ac814794ba801b0f95
Alexis Rodríguez <alexis.rcas@gmail.com>
2025-03-12 22:15:15 +0000

    Commit inicial

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function($) {
+
+    $('.level-bar-inner').css('width', '0');
+    $('.cap').css('opacity', 0.7);
+
+    $('.glow').on('click', (e) => {
+        $('.banner').toggleClass('fade');
+        setTimeout(() => {
+            $('.banner').toggleClass('fade');
+        }, 2500)
+    })
+
+    $('.fs-question').on('click', (e) => {
+        $('#fs-banner').removeClass('hidden');
+        $('#fs-banner').toggleClass('fade');
+    })
+    
+    $('.more-button').on('click', (e) => {
+    $('.more-courses').removeClass('hidden');
+    $('.more-courses').toggleClass('fade');
+    $('.more-button').toggleClass('hidden');
+})
+   
+
+});
